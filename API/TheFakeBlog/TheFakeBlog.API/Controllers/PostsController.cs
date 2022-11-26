955ec4d18e45c62e1e12117d9ece9d7b112ed0e7
tejass <Tejas.Sathe@centiro.com>
2022-11-26 22:16:23 +0530

    Add api controller

---
+++ API/TheFakeBlog/TheFakeBlog.API/Controllers/PostsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheFakeBlog.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostsController : Controller
+    {
+        public IActionResult FetAllPosts()
+        
+    }
+}
---
+++ API/TheFakeBlog/TheFakeBlog.API/Data/TheFakeBlogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheFakeBlog.API.Entities;
+
+namespace TheFakeBlog.API.Data
+{
+    public class TheFakeBlogDbContext : DbContext
+    {
+        public TheFakeBlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
