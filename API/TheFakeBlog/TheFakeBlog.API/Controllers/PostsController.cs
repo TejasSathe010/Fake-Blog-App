dbf692912bf91663105e4edba907654739da268b
tejass <Tejas.Sathe@centiro.com>
2022-11-29 23:06:37 +0530

    Add posts post request model

--- API/TheFakeBlog/TheFakeBlog.API/Controllers/PostsController.cs
+++ API/TheFakeBlog/TheFakeBlog.API/Controllers/PostsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheFakeBlog.API.Data;
 
 namespace TheFakeBlog.API.Controllers
 {
@@ -6,7 +8,37 @@ namespace TheFakeBlog.API.Controllers
     [Route("api/[controller]")]
     public class PostsController : Controller
     {
-        public IActionResult FetAllPosts()
+        private readonly TheFakeBlogDbContext dbContext;
+
+        public PostsController(TheFakeBlogDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPosts()
+        {
+            var posts = await dbContext.Posts.ToListAsync();
+            return Ok(posts);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetPostById(Guid id)
+        {
+            var posts = await dbContext.Posts.FirstOrDefaultAsync();
+            if(posts != null)
+            {
+                return Ok(posts);
+            }
+            return NotFound(); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost()
+        {
+
+        }
         
     }
 }
